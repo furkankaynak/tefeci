63b35ba0560a9adbc2ff88ae22f65634481a155b
furki <furkankaynak.74@gmail.com>
2013-05-01 00:22:09 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tefeci::Application.config.secret_token = '38c5a8d7be0d8e8d99de67ff2902f5cd2b6c5b1642c1080bad0719dc4d814477958f4e649e2e6b51528e750edf589e42d819643a95702f53e2957017067bd725'
